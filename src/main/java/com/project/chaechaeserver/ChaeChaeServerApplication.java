e0279a6f0717b4c792346fdde50aee7b6373ccf7
ohsutinn <114675677+ohsutinn@users.noreply.github.com>
2025-06-24 20:45:45 +0900

    [ init ] init commit

---
+++ src/main/java/com/project/chaechaeserver/ChaeChaeServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.chaechaeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaeChaeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaeChaeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/chaechaeserver/ChaeChaeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chaechaeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaeChaeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
