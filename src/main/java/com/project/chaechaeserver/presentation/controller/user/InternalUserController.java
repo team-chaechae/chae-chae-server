09af7e6d40067702d75a458d06238792fd42b231
ohsutinn <114675677+ohsutinn@users.noreply.github.com>
2025-06-25 14:39:21 +0900

    [ feat ] 회원가입 관리자 접근 제어 추가 ( #2 )

--- src/main/java/com/project/chaechaeserver/presentation/controller/user/InternalUserController.java
+++ src/main/java/com/project/chaechaeserver/presentation/controller/user/InternalUserController.java
@@ -3,15 +3,19 @@ package com.project.chaechaeserver.presentation.controller.user;
 import com.project.chaechaeserver.application.global.dto.ResDTO;
 import com.project.chaechaeserver.application.service.user.InternalUserService;
 import com.project.chaechaeserver.application.response.user.ResInternalUserPostSignupDTO;
+import com.project.chaechaeserver.domain.model.user.constraint.RoleType;
 import com.project.chaechaeserver.presentation.request.user.ReqInternalUserPostSignupDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.project.chaechaeserver.domain.model.user.constraint.RoleType.Role.ADMIN;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +25,7 @@ public class InternalUserController {
     private final InternalUserService internalUserService;
 
     @PostMapping("/signup")
+    @Secured(ADMIN)
     public ResponseEntity<ResDTO<ResInternalUserPostSignupDTO>> signup(@RequestBody @Valid ReqInternalUserPostSignupDTO dto) {
 
         return new ResponseEntity<>(
