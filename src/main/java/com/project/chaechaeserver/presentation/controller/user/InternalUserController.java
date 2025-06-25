b7ef0928699b7a730afc68bbb9697cae07cbd3f3
ohsutinn <114675677+ohsutinn@users.noreply.github.com>
2025-06-25 13:45:33 +0900

    [ feat ] 회원가입 API 및 서비스 로직 구현 ( #2 )

---
+++ src/main/java/com/project/chaechaeserver/presentation/controller/user/InternalUserController.java
@@ -0,0 +1,35 @@
+package com.project.chaechaeserver.presentation.controller.user;
+
+import com.project.chaechaeserver.application.global.dto.ResDTO;
+import com.project.chaechaeserver.application.user.service.InternalUserService;
+import com.project.chaechaeserver.application.user.response.ResInternalUserPostSignupDTO;
+import com.project.chaechaeserver.presentation.request.user.ReqInternalUserPostSignupDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class InternalUserController {
+
+    private final InternalUserService internalUserService;
+
+    @PostMapping
+    public ResponseEntity<ResDTO<ResInternalUserPostSignupDTO>> signup(@RequestBody @Valid ReqInternalUserPostSignupDTO dto) {
+
+        return new ResponseEntity<>(
+                ResDTO.<ResInternalUserPostSignupDTO>builder()
+                        .code(HttpStatus.CREATED.value())
+                        .message("회원가입에 성공하였습니다.")
+                        .data(internalUserService.signup(dto))
+                        .build(),
+                HttpStatus.CREATED
+        );
+    }
+}
