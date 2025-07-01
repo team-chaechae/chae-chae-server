65ad83d865bc77b1712599d0b813343924c02c4a
saven2762 <https://github.com/seven2762>
2025-07-02 03:57:17 +0900

    [Feat] product 응용계층 서비스 impl 클래스 생성

---
+++ src/main/java/com/project/chaechaeserver/application/service/products/ProductsServiceImpl.java
@@ -0,0 +1,44 @@
+package com.project.chaechaeserver.application.service.products;
+
+import com.project.chaechaeserver.application.response.products.ResCreateProductInfoDTO;
+import com.project.chaechaeserver.domain.model.products.ProductsEntity;
+import com.project.chaechaeserver.domain.repository.products.ProductsRepository;
+import com.project.chaechaeserver.domain.service.products.ProductDomainService;
+import com.project.chaechaeserver.presentation.request.products.ReqCreateProductsDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductsServiceImpl implements ProductsService {
+
+    private final ProductDomainService productDomainService;
+    private final ProductsRepository productsRepository;
+
+
+    @Override
+    @Transactional
+    public ResCreateProductInfoDTO createProductInfo(ReqCreateProductsDTO request) {
+
+        productDomainService.validateProductName(request.getProduct().getName());
+
+        ProductsEntity savedProduct = productsRepository.save(
+            ProductsEntity.createProducts(
+                request.getProduct().getName(),
+                request.getProduct().getCategory(),
+                request.getProduct().getPrice(),
+                request.getProduct().getUnit(),
+                false
+            )
+        );
+
+        return ResCreateProductInfoDTO.of(
+            savedProduct.getId(),
+            savedProduct.getName(),
+            savedProduct.getCategory(),
+            savedProduct.getPrice(),
+            savedProduct.getUnit()
+        );
+    }
+}
