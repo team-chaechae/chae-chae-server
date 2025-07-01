e84e584de322bd72edfa1409bb3e7c50e9160499
saven2762 <https://github.com/seven2762>
2025-07-02 03:56:51 +0900

    [Feat] product 도메인 서비스 impl 클래스 생성

---
+++ src/main/java/com/project/chaechaeserver/domain/service/products/ProductDomainServiceImpl.java
@@ -0,0 +1,19 @@
+package com.project.chaechaeserver.domain.service.products;
+
+import com.project.chaechaeserver.domain.repository.products.ProductsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ProductDomainServiceImpl implements ProductDomainService {
+
+    private final ProductsRepository productsRepository;
+
+    @Override
+    public void validateProductName(String name) {
+        if (productsRepository.existsByName(name)) {
+            throw new IllegalArgumentException("이미 존재하는 상품명입니다: " + name);
+        }
+    }
+}
