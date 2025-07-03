8cfb5c680f9a8fafaa97b9832837eba448462fc0
saven2762 <https://github.com/seven2762>
2025-07-03 21:46:38 +0900

    [refactor] 상품 생성 응답 dto 네이밍 변경으로 인한 수정

--- src/main/java/com/project/chaechaeserver/application/service/products/ProductsService.java
+++ src/main/java/com/project/chaechaeserver/application/service/products/ProductsService.java
@@ -1,9 +1,10 @@
 package com.project.chaechaeserver.application.service.products;
 
-import com.project.chaechaeserver.application.response.products.ResCreateProductInfoDTO;
+
+import com.project.chaechaeserver.application.response.products.ResCreateProductPostDTO;
 import com.project.chaechaeserver.presentation.request.products.ReqCreateProductsDTO;
 
 public interface ProductsService {
 
-    ResCreateProductInfoDTO createProductInfo(ReqCreateProductsDTO request);
+    ResCreateProductPostDTO createProductInfo(ReqCreateProductsDTO request);
 }
