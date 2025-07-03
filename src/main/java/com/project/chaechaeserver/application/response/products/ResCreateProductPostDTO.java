e1ecaefb2691737c53195a0d29346c9bf837cfaf
saven2762 <https://github.com/seven2762>
2025-07-03 21:45:47 +0900

    [refactor] 기존 응답 DTO 이름 변경 (새로 생성함)

---
+++ src/main/java/com/project/chaechaeserver/application/response/products/ResCreateProductPostDTO.java
@@ -0,0 +1,48 @@
+package com.project.chaechaeserver.application.response.products;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResCreateProductPostDTO {
+
+
+    private ProductInfo productInfo;
+
+    public static ResCreateProductPostDTO of(Long id ,String name, String category, int price, String unit) {
+        return ResCreateProductPostDTO.builder()
+            .productInfo(ProductInfo.from(id,name, category, price, unit))
+            .build();
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ProductInfo {
+
+        private Long id;
+        private String name;
+        private String category;
+        private int price;
+        private String unit;
+
+
+        public static ProductInfo from(Long id, String name, String category, int price, String unit) {
+            return ProductInfo.builder()
+                .id(id)
+                .name(name)
+                .category(category)
+                .price(price)
+                .unit(unit)
+                .build();
+
+        }
+    }
+}
