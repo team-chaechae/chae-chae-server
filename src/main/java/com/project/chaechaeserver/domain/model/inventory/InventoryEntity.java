bdf7a62bf70034639a3162eefe540a239ff81c34
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-07-05 02:42:03 +0900

    [ feat ] 재고 엔티티 클래스 추가 (#29)

---
+++ src/main/java/com/project/chaechaeserver/domain/model/inventory/InventoryEntity.java
@@ -0,0 +1,65 @@
+package com.project.chaechaeserver.domain.model.inventory;
+
+import com.project.chaechaeserver.domain.model.products.ProductsEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Table(name = "inventory")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class InventoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inventory_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "product_id")
+    private ProductsEntity products;
+
+    @Column(name = "quantity" , nullable = false)
+    private Integer quantity;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Builder
+    public InventoryEntity(ProductsEntity products, Integer quantity) {
+        this.products = products;
+        this.quantity = quantity;
+    }
+
+    public static InventoryEntity createInventory(ProductsEntity products, Integer quantity) {
+        return InventoryEntity.builder()
+            .products(products)
+            .quantity(quantity)
+            .build();
+    }
+    public void updateInventory(ProductsEntity products, Integer quantity) {
+        this.products = products;
+    }
+}
