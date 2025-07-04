04e0f53fdb59c246bd6fd678b39fac429523857a
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-07-05 02:41:21 +0900

    [ feat ] 재고 응용 계층 서비스 구현체 클래스 추가 (#29)

---
+++ src/main/java/com/project/chaechaeserver/application/service/inventory/InventoryServiceImpl.java
@@ -0,0 +1,38 @@
+package com.project.chaechaeserver.application.service.inventory;
+
+
+import com.project.chaechaeserver.application.response.inventory.ResCreateInventoryPostDTO;
+import com.project.chaechaeserver.domain.model.inventory.InventoryEntity;
+import com.project.chaechaeserver.domain.model.products.ProductsEntity;
+import com.project.chaechaeserver.domain.repository.inventory.InventoryRepository;
+import com.project.chaechaeserver.domain.repository.products.ProductsRepository;
+import com.project.chaechaeserver.presentation.request.inventory.ReqCreateInventoryDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryServiceImpl implements InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+    private final ProductsRepository productsRepository;
+
+    @Transactional
+    public ResCreateInventoryPostDTO createInventory(ReqCreateInventoryDTO request) {
+
+
+        ProductsEntity product = productsRepository.findById(request.getInventory().getProductId())
+            .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
+
+        InventoryEntity savedInventory = inventoryRepository.save(
+            InventoryEntity.createInventory(
+                product,
+                request.getInventory().getQuantity()
+            )
+        );
+
+        return ResCreateInventoryPostDTO.of(product.getId(), savedInventory.getId(),
+            savedInventory.getQuantity());
+    }
+}
