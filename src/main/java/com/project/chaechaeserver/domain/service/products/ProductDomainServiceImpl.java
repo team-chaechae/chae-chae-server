6ba9629f2a291801849f34244507bf122ea69238
Hujin0322 <hongyuj0322@naver.com>
2025-07-06 17:54:19 +0900

    [feat] 발주용 상품 가격 조회 기능 추가 (#20)

--- src/main/java/com/project/chaechaeserver/domain/service/products/ProductDomainServiceImpl.java
+++ src/main/java/com/project/chaechaeserver/domain/service/products/ProductDomainServiceImpl.java
@@ -16,4 +16,12 @@ public class ProductDomainServiceImpl implements ProductDomainService {
             throw new IllegalArgumentException("이미 존재하는 상품명입니다: " + name);
         }
     }
+
+    // 발주용 상품 가격 확인
+    @Override
+    public int getUnitPrice(Long productId) {
+        return productsRepository.findById(productId)
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상품입니다."))
+            .getPrice();
+    }
 }
