7e8bd5f31c5755d09a8eb33d9230e8202c2e072e
ohsutinn <114675677+ohsutinn@users.noreply.github.com>
2025-07-06 12:54:00 +0900

    [ feat ] 판매 검색 동적 쿼리 구현 ( #27 )

--- src/main/java/com/project/chaechaeserver/domain/repository/sales/SalesRepository.java
+++ src/main/java/com/project/chaechaeserver/domain/repository/sales/SalesRepository.java
@@ -1,9 +1,17 @@
 package com.project.chaechaeserver.domain.repository.sales;
 
 import com.project.chaechaeserver.domain.model.sales.SalesEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SalesRepository {
 
     SalesEntity getSalesBySalesId(Long salesId);
+    Page<SalesEntity> findSalesByDeletedAtIsNullWithCondition(Pageable pageable, Boolean deletedCond, String productName,
+                                                                     LocalDate startDate, LocalDate endDate, LocalDate exactDate,
+                                                                     List<String> sortList);
 
 }
