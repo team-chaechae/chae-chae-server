03dbc4e1996b29ebd1cd77d44b60a2200ac33fe2
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-07-09 04:33:52 +0900

    [ refactor ] 인벤토리 연관관계 수정 및 도메인 로직 변경(inventory)(#33)

--- src/main/java/com/project/chaechaeserver/domain/model/inventory/InventoryEntity.java
+++ src/main/java/com/project/chaechaeserver/domain/model/inventory/InventoryEntity.java
@@ -3,6 +3,7 @@ package com.project.chaechaeserver.domain.model.inventory;
 import com.project.chaechaeserver.domain.model.products.ProductEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,7 +30,7 @@ public class InventoryEntity {
     @Column(name = "inventory_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private ProductEntity product;
 
@@ -53,4 +54,13 @@ public class InventoryEntity {
         this.quantity = quantity;
     }
 
+    public static InventoryEntity createInventory(ProductEntity product, Integer quantity) {
+        return InventoryEntity.builder()
+            .product(product)
+            .quantity(quantity)
+            .build();
+    }
+    public void updateInventory(ProductEntity products, Integer quantity) {
+        this.product = products;
+    }
 }
