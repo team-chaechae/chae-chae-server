ec401e1c45b0347112f709a6a48ebb465c7db990
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-07-09 04:37:57 +0900

    [ refactor ]  상품 조회  메소드  구현 추가 (#33)

--- src/main/java/com/project/chaechaeserver/application/service/products/ProductsServiceImpl.java
+++ src/main/java/com/project/chaechaeserver/application/service/products/ProductsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.project.chaechaeserver.application.service.products;
 
 import com.project.chaechaeserver.application.response.products.ResCreateProductPostDTO;
+import com.project.chaechaeserver.application.response.products.ResGetProductDTO;
 import com.project.chaechaeserver.domain.model.products.ProductEntity;
 import com.project.chaechaeserver.domain.repository.products.ProductsRepository;
 import com.project.chaechaeserver.domain.service.products.ProductDomainService;
@@ -19,16 +20,16 @@ public class ProductsServiceImpl implements ProductsService {
 
     @Override
     @Transactional
-    public ResCreateProductPostDTO createProductInfo(ReqCreateProductsDTO request) {
+    public ResCreateProductPostDTO createProductInfo(ReqCreateProductsDTO dto) {
 
-        productDomainService.validateProductName(request.getProduct().getName());
+        productDomainService.validateProductName(dto.getProduct().getName());
 
         ProductEntity savedProduct = productsRepository.save(
             ProductEntity.createProducts(
-                request.getProduct().getName(),
-                request.getProduct().getCategory(),
-                request.getProduct().getPrice(),
-                request.getProduct().getUnit()
+                dto.getProduct().getName(),
+                dto.getProduct().getCategory(),
+                dto.getProduct().getPrice(),
+                dto.getProduct().getUnit()
             )
         );
 
@@ -36,4 +37,14 @@ public class ProductsServiceImpl implements ProductsService {
         savedProduct
         );
     }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ResGetProductDTO getProductInfo(Long productId) {
+        ProductEntity product = productsRepository.findById(productId)
+            .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
+
+        return ResGetProductDTO.from(product);
+    }
 }
+
