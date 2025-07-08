532924e6086eedbf7da36c393c34264d4c1d0f2d
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-07-09 00:48:45 +0900

    [ refactor ] res dto 메소드 네이밍 변경으로 인한 서비스 로직 리턴 메소드 변경(#33)

--- src/main/java/com/project/chaechaeserver/application/service/inventory/InventoryServiceImpl.java
+++ src/main/java/com/project/chaechaeserver/application/service/inventory/InventoryServiceImpl.java
@@ -3,7 +3,7 @@ package com.project.chaechaeserver.application.service.inventory;
 
 import com.project.chaechaeserver.application.response.inventory.ResCreateInventoryPostDTO;
 import com.project.chaechaeserver.domain.model.inventory.InventoryEntity;
-import com.project.chaechaeserver.domain.model.products.ProductsEntity;
+import com.project.chaechaeserver.domain.model.products.ProductEntity;
 import com.project.chaechaeserver.domain.repository.inventory.InventoryRepository;
 import com.project.chaechaeserver.domain.repository.products.ProductsRepository;
 import com.project.chaechaeserver.presentation.request.inventory.ReqCreateInventoryDTO;
@@ -22,7 +22,7 @@ public class InventoryServiceImpl implements InventoryService {
     public ResCreateInventoryPostDTO createInventory(ReqCreateInventoryDTO request) {
 
 
-        ProductsEntity product = productsRepository.findById(request.getInventory().getProductId())
+        ProductEntity product = productsRepository.findById(request.getInventory().getProductId())
             .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
 
         InventoryEntity savedInventory = inventoryRepository.save(
@@ -32,7 +32,6 @@ public class InventoryServiceImpl implements InventoryService {
             )
         );
 
-        return ResCreateInventoryPostDTO.of(product.getId(), savedInventory.getId(),
-            savedInventory.getQuantity());
+        return ResCreateInventoryPostDTO.from(savedInventory);
     }
 }
