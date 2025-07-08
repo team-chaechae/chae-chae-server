d7d97340a85a573d722425447c2ec604509c3368
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-07-09 04:34:48 +0900

    [ refactor ] 인벤토리 서비스 인터페이스 메소드 명 변경 및 감소 메소드 구현 추가(#33)

--- src/main/java/com/project/chaechaeserver/application/service/inventory/InventoryServiceImpl.java
+++ src/main/java/com/project/chaechaeserver/application/service/inventory/InventoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.project.chaechaeserver.application.service.inventory;
 
 
 import com.project.chaechaeserver.application.response.inventory.ResCreateInventoryPostDTO;
+import com.project.chaechaeserver.application.response.inventory.ResUpdateInventoryPostDTO;
 import com.project.chaechaeserver.domain.model.inventory.InventoryEntity;
 import com.project.chaechaeserver.domain.model.products.ProductEntity;
 import com.project.chaechaeserver.domain.repository.inventory.InventoryRepository;
@@ -19,19 +20,26 @@ public class InventoryServiceImpl implements InventoryService {
     private final ProductsRepository productsRepository;
 
     @Transactional
-    public ResCreateInventoryPostDTO createInventory(ReqCreateInventoryDTO request) {
+    public ResCreateInventoryPostDTO addInventory(ReqCreateInventoryDTO dto) {
 
-
-        ProductEntity product = productsRepository.findById(request.getInventory().getProductId())
+        ProductEntity product = productsRepository.findById(dto.getInventory().getProductId())
             .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
 
-        InventoryEntity savedInventory = inventoryRepository.save(
-            InventoryEntity.createInventory(
-                product,
-                request.getInventory().getQuantity()
-            )
-        );
+        InventoryEntity savedInventory = product.addInventory(dto.getInventory().getQuantity());
 
         return ResCreateInventoryPostDTO.from(savedInventory);
     }
+
+    @Override
+    @Transactional
+    public ResUpdateInventoryPostDTO decreaseInventory(ReqCreateInventoryDTO dto) {
+
+        ProductEntity product = productsRepository.findById(dto.getInventory().getProductId())
+            .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
+
+        InventoryEntity savedInventory = product.decreaseInventory(dto.getInventory().getQuantity());
+
+        return ResUpdateInventoryPostDTO.from(savedInventory);
+    }
+
 }
