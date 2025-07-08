5525fc25edad1868609d973ca7dd32411f44777a
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-07-09 00:51:32 +0900

    [ refactor ] of -> from 으로 변경으로 인한 서비스 로직 반환 메서드 변경(product)(#33)

--- src/main/java/com/project/chaechaeserver/application/service/products/ProductsServiceImpl.java
+++ src/main/java/com/project/chaechaeserver/application/service/products/ProductsServiceImpl.java
@@ -1,7 +1,7 @@
 package com.project.chaechaeserver.application.service.products;
 
 import com.project.chaechaeserver.application.response.products.ResCreateProductPostDTO;
-import com.project.chaechaeserver.domain.model.products.ProductsEntity;
+import com.project.chaechaeserver.domain.model.products.ProductEntity;
 import com.project.chaechaeserver.domain.repository.products.ProductsRepository;
 import com.project.chaechaeserver.domain.service.products.ProductDomainService;
 import com.project.chaechaeserver.presentation.request.products.ReqCreateProductsDTO;
@@ -23,8 +23,8 @@ public class ProductsServiceImpl implements ProductsService {
 
         productDomainService.validateProductName(request.getProduct().getName());
 
-        ProductsEntity savedProduct = productsRepository.save(
-            ProductsEntity.createProducts(
+        ProductEntity savedProduct = productsRepository.save(
+            ProductEntity.createProducts(
                 request.getProduct().getName(),
                 request.getProduct().getCategory(),
                 request.getProduct().getPrice(),
@@ -32,12 +32,8 @@ public class ProductsServiceImpl implements ProductsService {
             )
         );
 
-        return ResCreateProductPostDTO.of(
-            savedProduct.getId(),
-            savedProduct.getName(),
-            savedProduct.getCategory(),
-            savedProduct.getPrice(),
-            savedProduct.getUnit()
+        return ResCreateProductPostDTO.from(
+        savedProduct
         );
     }
 }
