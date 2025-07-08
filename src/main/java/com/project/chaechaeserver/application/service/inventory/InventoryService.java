51a8059483a1694e4e28e8a6abd52f2d125c9b40
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-07-09 04:34:32 +0900

    [ refactor ] 인벤토리 서비스 인터페이스 메소드 명 변경 및 감소 메소드 추가(#33)

--- src/main/java/com/project/chaechaeserver/application/service/inventory/InventoryService.java
+++ src/main/java/com/project/chaechaeserver/application/service/inventory/InventoryService.java
@@ -1,9 +1,12 @@
 package com.project.chaechaeserver.application.service.inventory;
 
 import com.project.chaechaeserver.application.response.inventory.ResCreateInventoryPostDTO;
+import com.project.chaechaeserver.application.response.inventory.ResUpdateInventoryPostDTO;
 import com.project.chaechaeserver.presentation.request.inventory.ReqCreateInventoryDTO;
 
 public interface InventoryService {
 
-    ResCreateInventoryPostDTO createInventory(ReqCreateInventoryDTO request);
+    ResCreateInventoryPostDTO addInventory(ReqCreateInventoryDTO dto);
+
+    ResUpdateInventoryPostDTO decreaseInventory(ReqCreateInventoryDTO dto);
 }
