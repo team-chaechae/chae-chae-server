45906076a01c7976ef9e9b4e8b1502bf30e86527
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-07-09 00:51:03 +0900

    [ refactor ] of -> from 으로 변경, 파라미터 인자 엔티티로 변경(inventory)(#33)

--- src/main/java/com/project/chaechaeserver/application/response/inventory/ResCreateInventoryPostDTO.java
+++ src/main/java/com/project/chaechaeserver/application/response/inventory/ResCreateInventoryPostDTO.java
@@ -1,6 +1,7 @@
 package com.project.chaechaeserver.application.response.inventory;
 
 
+import com.project.chaechaeserver.domain.model.inventory.InventoryEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,9 +15,9 @@ public class ResCreateInventoryPostDTO {
 
     private InventoryInfo inventoryInfo;
 
-    public static ResCreateInventoryPostDTO of(Long id, Long productId, Integer quantity) {
+    public static ResCreateInventoryPostDTO from(InventoryEntity inventoryEntity) {
         return ResCreateInventoryPostDTO.builder()
-            .inventoryInfo(InventoryInfo.from(id, productId, quantity))
+            .inventoryInfo(InventoryInfo.from(inventoryEntity))
             .build();
     }
 
@@ -30,11 +31,11 @@ public class ResCreateInventoryPostDTO {
         private Integer quantity;
 
 
-        public static InventoryInfo from(Long id, Long productId, Integer quantity) {
+        public static InventoryInfo from(InventoryEntity inventoryEntity) {
             return InventoryInfo.builder()
-                .inventoryId(id)
-                .productId(productId)
-                .quantity(quantity)
+                .inventoryId(inventoryEntity.getId())
+                .productId(inventoryEntity.getProducts().getId())
+                .quantity(inventoryEntity.getQuantity())
                 .build();
         }
 
