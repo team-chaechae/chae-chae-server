f278bd8d54166d18c94c0e5e21da78593fd722b0
ohsutinn <114675677+ohsutinn@users.noreply.github.com>
2025-07-14 15:18:38 +0900

    [ feat ] Swagger 응답 스키마 자동 Wrapping 기능 추가 ( #42 )
    - OperationCustomizer를 활용하여 Swagger 200 응답 스키마를 공통 응답 포맷(ResDTO)으로 감싸도록 설정
    
    - 응답 스키마를 `code`, `message`, `data` 속성으로 자동 래핑하여 일관된 API 문서 구조 제공
    
    - Swagger UI에서 Try 이전 예시 값에도 전체 응답 구조가 표시되도록 개선

--- src/main/java/com/project/chaechaeserver/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/project/chaechaeserver/infrastructure/config/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package com.project.chaechaeserver.infrastructure.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,4 +34,33 @@ public class SwaggerConfig {
                 .addSecurityItem(securityRequirement)
                 .schemaRequirement("BearerAuth", securityScheme);
     }
+
+    @Bean
+    public OperationCustomizer operationCustomizer() {
+        return (operation, handlerMethod) -> {
+            this.addResponseBodyWrapperSchemaExample(operation);
+            return operation;
+        };
+    }
+
+    private void addResponseBodyWrapperSchemaExample(Operation operation) {
+        final Content content = operation.getResponses().get("200").getContent();
+        if (content != null) {
+            content.forEach((mediaTypeKey, mediaType) -> {
+                Schema<?> originalSchema = mediaType.getSchema();
+                Schema<?> wrappedSchema = wrapSchema(originalSchema);
+                mediaType.setSchema(wrappedSchema);
+            });
+        }
+    }
+
+    private Schema<?> wrapSchema(Schema<?> originalSchema) {
+        final Schema<?> wrapperSchema = new Schema<>();
+        wrapperSchema.addProperty("code", new Schema<>().type("integer").example(200));
+        wrapperSchema.addProperty("message", new Schema<>().type("string").example("요청에 성공하였습니다"));
+        wrapperSchema.addProperty("data", originalSchema);
+
+        return wrapperSchema;
+    }
+
 }
