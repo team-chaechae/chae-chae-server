be586a278ccacb18d190c45625ef001b1b3d33ad
ohsutinn <114675677+ohsutinn@users.noreply.github.com>
2025-07-14 13:50:22 +0900

    [ chore ] Swagger에 JWT 인증 설정 추가 ( #42 )
    - Authorization 헤더를 통해 JWT 토큰을 입력할 수 있도록 설정

---
+++ src/main/java/com/project/chaechaeserver/infrastructure/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.project.chaechaeserver.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        // Security Scheme 정의
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // Security Requirement 정의
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
+        return new OpenAPI()
+                .info(new Info().title("CHAE-CHAE API")
+                        .description("Chae-Chae Application API Documentation")
+                        .version("v1.0"))
+                .addSecurityItem(securityRequirement)
+                .schemaRequirement("BearerAuth", securityScheme);
+    }
+}
