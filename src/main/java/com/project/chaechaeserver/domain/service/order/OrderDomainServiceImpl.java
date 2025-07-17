a61068d9e78fd7bae5a28609baf4da9081f17798
Hujin0322 <hongyuj0322@naver.com>
2025-07-17 22:01:28 +0900

    [ refactor ] 발주 중복 확인을 위한 상태값을 'APPROVED'로 수정 (#50)

--- src/main/java/com/project/chaechaeserver/domain/service/order/OrderDomainServiceImpl.java
+++ src/main/java/com/project/chaechaeserver/domain/service/order/OrderDomainServiceImpl.java
@@ -13,8 +13,8 @@ public class OrderDomainServiceImpl implements OrderDomainService {
 
   @Override
   public void validateDuplicateOrder(Long productId) {
-    if (orderRepository.existsByProductIdAndStatus(productId, StatusType.REQUESTED)) {
-      throw new IllegalArgumentException("이미 발주 요청 중인 상품입니다.");
+    if (orderRepository.existsByProductIdAndStatus(productId, StatusType.APPROVED)) {
+      throw new IllegalArgumentException("이미 발주 중인 상품입니다.");
     }
   }
 
