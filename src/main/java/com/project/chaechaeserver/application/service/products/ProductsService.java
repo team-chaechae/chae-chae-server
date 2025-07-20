6bd58388bbd44fd0bd82e2805d64a001a5946163
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-07-20 22:19:21 +0900

    [ feat ] 상품 단일 조회 및 페이징 메서드 추가  (#49)

--- src/main/java/com/project/chaechaeserver/application/service/products/ProductsService.java
+++ src/main/java/com/project/chaechaeserver/application/service/products/ProductsService.java
@@ -2,11 +2,21 @@ package com.project.chaechaeserver.application.service.products;
 
 
 import com.project.chaechaeserver.application.response.products.ResCreateProductPostDTO;
-import com.project.chaechaeserver.application.response.products.ResGetProductDTO;
+import com.project.chaechaeserver.application.response.products.ResGetProductWithOrderStatus;
+import com.project.chaechaeserver.application.response.products.ResProductSearchWithOrderStatusDTO;
+import com.project.chaechaeserver.domain.model.products.constraint.ProductStatusType;
 import com.project.chaechaeserver.presentation.request.products.ReqCreateProductsDTO;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductsService {
 
     ResCreateProductPostDTO createProductInfo(ReqCreateProductsDTO request);
-    ResGetProductDTO getProductInfo(Long productId);
+    ResGetProductWithOrderStatus getProductInfo(Long productId);
+    ResProductSearchWithOrderStatusDTO getProductSearchInfo(
+        Pageable pageable,
+        String productName, Boolean deletedAt, ProductStatusType productStatus, ProductStatusType.ProductOrderType orderStatus , LocalDate startDate,
+        LocalDate endDate, LocalDate exactDate
+        , List<String> sortList);
 }
