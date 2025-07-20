525f2ab703f7ffec034be219d18e9605900d42b4
Hujin0322 <hongyuj0322@naver.com>
2025-07-21 00:01:25 +0900

    [ feat ] 발주 목록 검색 API 구현 (#34)

--- src/main/java/com/project/chaechaeserver/presentation/controller/order/OrderController.java
+++ src/main/java/com/project/chaechaeserver/presentation/controller/order/OrderController.java
@@ -4,16 +4,26 @@ import static com.project.chaechaeserver.domain.model.user.constraint.RoleType.R
 
 import com.project.chaechaeserver.application.global.dto.ResDTO;
 import com.project.chaechaeserver.application.response.order.ResCreateOrderPostDTO;
+import com.project.chaechaeserver.application.response.order.ResOrdersSearchDTO;
 import com.project.chaechaeserver.application.service.order.OrderService;
+import com.project.chaechaeserver.domain.model.order.constraint.StatusType;
 import com.project.chaechaeserver.presentation.request.order.ReqCreateOrderDTO;
 import jakarta.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,6 +44,31 @@ public class OrderController {
             .data(orderService.createOrderInfo(dto))
             .build(),
         HttpStatus.CREATED
+    );
+  }
+
+  @GetMapping
+  @Secured(ADMIN)
+  public ResponseEntity<ResDTO<ResOrdersSearchDTO>> searchOrdersByFilter(
+      @RequestParam(required = false) Long orderId,
+      @RequestParam(required = false) String productName,
+      @RequestParam(required = false) String status,
+      @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+      @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+      @RequestParam(required = false) List<String> sort,
+      @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+
+    StatusType statusType = StatusType.from(status);
+
+    return new ResponseEntity<>(
+        ResDTO.<ResOrdersSearchDTO>builder()
+            .code(HttpStatus.OK.value())
+            .message("발주 기록 검색 성공")
+            .data(orderService.searchOrdersByFilter(
+                pageable, orderId, productName, statusType, startDate, endDate, sort
+            ))
+            .build(),
+        HttpStatus.OK
     );
   }
 }
