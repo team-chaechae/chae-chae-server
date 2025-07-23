256c44d7092cad8884bd5140c6916f36e1aaf409
Hujin0322 <hongyuj0322@naver.com>
2025-07-23 19:55:07 +0900

    [ feat ] 발주 상태 변경 요청 DTO 추가 (#56)

---
+++ src/main/java/com/project/chaechaeserver/presentation/request/order/ReqUpdateOrderDTO.java
@@ -0,0 +1,20 @@
+package com.project.chaechaeserver.presentation.request.order;
+
+import com.project.chaechaeserver.domain.model.order.constraint.StatusType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReqUpdateOrderDTO {
+
+    @Valid
+    @NotNull(message = "변경된 발주 상태를 입력해주세요.")
+    private StatusType status;
+}
