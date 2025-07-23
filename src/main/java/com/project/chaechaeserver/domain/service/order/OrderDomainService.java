16b71730e62a4d97d9d31078961de1bce071f016
Hujin0322 <hongyuj0322@naver.com>
2025-07-23 19:56:54 +0900

    [ feat ] 발주 조회 및 완료 상태 중복 검증 로직 추가 (#56)

--- src/main/java/com/project/chaechaeserver/domain/service/order/OrderDomainService.java
+++ src/main/java/com/project/chaechaeserver/domain/service/order/OrderDomainService.java
@@ -1,5 +1,13 @@
 package com.project.chaechaeserver.domain.service.order;
 
+import com.project.chaechaeserver.domain.model.order.OrderEntity;
+import com.project.chaechaeserver.domain.model.order.constraint.StatusType;
+
 public interface OrderDomainService {
-  void validateDuplicateOrder(Long productId);
+
+    void validateDuplicateOrder(Long productId);
+
+    OrderEntity findById(Long orderId);
+
+    void validateAlreadyCompleted(OrderEntity order, StatusType status);
 }
--- src/main/java/com/project/chaechaeserver/domain/service/order/OrderDomainServiceImpl.java
+++ src/main/java/com/project/chaechaeserver/domain/service/order/OrderDomainServiceImpl.java
@@ -1,6 +1,8 @@
 package com.project.chaechaeserver.domain.service.order;
 
 import com.project.chaechaeserver.application.global.excepion.EntityAlreadyExistException;
+import com.project.chaechaeserver.application.global.excepion.EntityNotFoundException;
+import com.project.chaechaeserver.domain.model.order.OrderEntity;
 import com.project.chaechaeserver.domain.model.order.constraint.StatusType;
 import com.project.chaechaeserver.domain.repository.order.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +12,26 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderDomainServiceImpl implements OrderDomainService {
 
-  private final OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
 
-  @Override
-  public void validateDuplicateOrder(Long productId) {
-    if (orderRepository.existsByProductInfo_ProductIdAndStatus(productId, StatusType.APPROVED)) {
-      throw new EntityAlreadyExistException("이미 발주 중인 상품입니다.");
+    @Override
+    public void validateDuplicateOrder(Long productId) {
+        if (orderRepository.existsByProductInfo_ProductIdAndStatus(productId, StatusType.APPROVED)) {
+            throw new EntityAlreadyExistException("이미 발주 중인 상품입니다.");
+        }
     }
-  }
+
+    @Override
+    public OrderEntity findById(Long orderId) {
+        return orderRepository.findById(orderId)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 발주입니다."));
+    }
+
+    @Override
+    public void validateAlreadyCompleted(OrderEntity order, StatusType targetStatus) {
+        if (order.getStatus() == StatusType.COMPLETED && targetStatus == StatusType.COMPLETED) {
+            throw new EntityAlreadyExistException("이미 발주 완료된 상품입니다.");
+        }
+    }
 
 }
