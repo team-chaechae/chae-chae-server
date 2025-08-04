fb57dfb1145b0dc4c20738664161f777b9c505c0
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-08-04 20:44:34 +0900

    [ feat ] 벌크 메서드 인터페이스 메서드 추가 (#55)

--- src/main/java/com/project/chaechaeserver/application/service/inventory/InventoryService.java
+++ src/main/java/com/project/chaechaeserver/application/service/inventory/InventoryService.java
@@ -1,12 +1,35 @@
 package com.project.chaechaeserver.application.service.inventory;
 
-import com.project.chaechaeserver.application.response.inventory.ResCreateInventoryPostDTO;
-import com.project.chaechaeserver.application.response.inventory.ResUpdateInventoryPostDTO;
-import com.project.chaechaeserver.presentation.request.inventory.ReqCreateInventoryDTO;
+import com.project.chaechaeserver.application.response.inventory.ResGetInventoryDTO;
+import com.project.chaechaeserver.application.response.inventory.ResInventorySearchDTO;
+import com.project.chaechaeserver.application.response.inventory.ResSingleUpdateInventoryDTO;
+import com.project.chaechaeserver.application.response.inventory.ResVulkCreateInventoryPostDTO;
+import com.project.chaechaeserver.application.response.inventory.ResVulkSaleInventoryDTO;
+import com.project.chaechaeserver.application.response.inventory.ResVulkUpdateInventoryDTO;
+import com.project.chaechaeserver.domain.model.products.constraint.ProductStatusType;
+import com.project.chaechaeserver.presentation.request.inventory.ReqSingleUpdateInventoryDTO;
+import com.project.chaechaeserver.presentation.request.inventory.ReqVulkCreateInventoryDTO;
+import com.project.chaechaeserver.presentation.request.inventory.ReqVulkUpdateInventoryDTO;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface InventoryService {
+    ResVulkCreateInventoryPostDTO createInventoryForVulk(ReqVulkCreateInventoryDTO dto);
 
-    ResCreateInventoryPostDTO addInventory(ReqCreateInventoryDTO dto);
+    ResSingleUpdateInventoryDTO modifyInventoryForSingle(ReqSingleUpdateInventoryDTO dto);
+    ResVulkSaleInventoryDTO decreaseInventoryForBulk(ReqVulkUpdateInventoryDTO dto);
+    ResVulkUpdateInventoryDTO modifyInventoryForVulk(ReqVulkUpdateInventoryDTO dto);
 
-    ResUpdateInventoryPostDTO decreaseInventory(ReqCreateInventoryDTO dto);
+    ResGetInventoryDTO getInventoryInfo(Long id);
+
+    ResInventorySearchDTO getInventorySearchInfo(Pageable pageable,
+        String productName,
+        Boolean deletedAt,
+        ProductStatusType productStatus,
+        Long productId,
+        LocalDate startDate,
+        LocalDate endDate,
+        LocalDate exactDate,
+        List<String> sortList);
 }
