40f079daf9f05c5adc081e1d733a2c09ffd9da78
ohsutinn <114675677+ohsutinn@users.noreply.github.com>
2025-09-24 22:21:17 +0900

    [ docs ] 챗봇 API Swagger 문서화 ( #66 )

--- src/main/java/com/project/chaechaeserver/application/response/chatbot/ResChatbotPostAnswerDTO.java
+++ src/main/java/com/project/chaechaeserver/application/response/chatbot/ResChatbotPostAnswerDTO.java
@@ -1,5 +1,6 @@
 package com.project.chaechaeserver.application.response.chatbot;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ResChatbotPostAnswerDTO {
 
+    @Schema(example = "시금치는 냉장보관을 합니다.")
     private String answer;
 
 }
--- src/main/java/com/project/chaechaeserver/presentation/request/chatbot/ReqChatbotPostQuestionDTO.java
+++ src/main/java/com/project/chaechaeserver/presentation/request/chatbot/ReqChatbotPostQuestionDTO.java
@@ -1,5 +1,6 @@
 package com.project.chaechaeserver.presentation.request.chatbot;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ReqChatbotPostQuestionDTO {
 
+    @Schema(example = "시금치의 보관방법을 알려주세요")
     @NotBlank(message = "질문을 입력해주세요.")
     private String question;
 
