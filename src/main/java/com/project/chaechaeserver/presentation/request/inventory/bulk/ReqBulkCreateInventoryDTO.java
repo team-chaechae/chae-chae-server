a3cff8825ec91be558b97f488173499839e99bd3
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-11-17 22:30:30 +0900

    [ feat ] 기존 bulk 패키지 추가 및 재고 벌크 생성 요청 dto 생성

---
+++ src/main/java/com/project/chaechaeserver/presentation/request/inventory/bulk/ReqBulkCreateInventoryDTO.java
@@ -0,0 +1,28 @@
+package com.project.chaechaeserver.presentation.request.inventory.bulk;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReqBulkCreateInventoryDTO {
+
+    private List<Inventory> inventory;
+
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Inventory {
+
+        private Long productId;
+        private Integer quantity;
+
+    }
+}
