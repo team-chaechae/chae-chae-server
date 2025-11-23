2c1a1cc9c7573a76ade5cbdbde55f091b1886354
seven2762 <eunbyeol@eunbyeol-ui-Macmini.local>
2025-11-23 18:23:29 +0900

    [ feat ]InventoryRepositoryImpl.java 상품 재고 도메인 분리로 인한 재고 조회 메서드 추가, 벌크 인서트 메서드 추가

--- src/main/java/com/project/chaechaeserver/infrastructure/inventory/InventoryRepositoryImpl.java
+++ src/main/java/com/project/chaechaeserver/infrastructure/inventory/InventoryRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.project.chaechaeserver.infrastructure.inventory;
 
+import com.project.chaechaeserver.application.response.inventory.InventoryWithProductDto;
 import com.project.chaechaeserver.domain.model.inventory.InventoryEntity;
 import com.project.chaechaeserver.domain.model.products.constraint.ProductStatusType;
 import com.project.chaechaeserver.domain.repository.inventory.InventoryRepository;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +18,7 @@ public class InventoryRepositoryImpl implements InventoryRepository {
 
     private final JpaInventoryRepository jpaInventoryRepository;
     private final InventoryQueryRepository inventoryQueryRepository;
+    private final JdbcInventoryRepository jdbcInventoryRepository;
 
     @Override
     public InventoryEntity save(InventoryEntity inventoryEntity) {
@@ -24,7 +27,8 @@ public class InventoryRepositoryImpl implements InventoryRepository {
 
     @Override
     public List<InventoryEntity> saveAll(List<InventoryEntity> inventoryEntities) {
-        return jpaInventoryRepository.saveAll(inventoryEntities);
+        jdbcInventoryRepository.bulkInsert(inventoryEntities);
+        return inventoryEntities;
     }
 
     @Override
@@ -33,11 +37,34 @@ public class InventoryRepositoryImpl implements InventoryRepository {
     }
 
     @Override
-    public Page<InventoryEntity> findInventoryByDeletedAtIsNullWithCondition(Pageable pageable,
-        String productName, Boolean deletedAt, ProductStatusType productStatus, Long productId,
-        LocalDate startDate, LocalDate endDate, LocalDate exactDate, List<String> sortList) {
-        return inventoryQueryRepository.findInventoryWithCondition(
-            pageable,productName, deletedAt, productStatus, productId, startDate, endDate, exactDate, sortList);
+    public InventoryWithProductDto findInventoryWithProductById(Long id) {
+        return inventoryQueryRepository.findInventoryWithProductById(id);
+    }
+
+    @Override
+    public Page<InventoryWithProductDto> findInventoryWithProduct(
+        Pageable pageable,
+        String productName,
+        Boolean deletedAt,
+        ProductStatusType productStatus,
+        Long productId,
+        LocalDate startDate,
+        LocalDate endDate,
+        LocalDate exactDate,
+        List<String> sortList) {
+        return inventoryQueryRepository.findInventoryWithProduct(
+            pageable, productName, deletedAt, productStatus, productId,
+            startDate, endDate, exactDate, sortList);
+    }
+
+    @Override
+    public Integer getCurrentStock(Long productId) {
+        return inventoryQueryRepository.getCurrentStock(productId);
+    }
+
+    @Override
+    public Map<Long, Integer> getCurrentStockMap(List<Long> productIds) {
+        return inventoryQueryRepository.getCurrentStockMap(productIds);
     }
 
 }
