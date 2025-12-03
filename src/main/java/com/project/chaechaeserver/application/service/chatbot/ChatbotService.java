6d4c7961f10bbfce47e61879fe56443911ba7de7
seven2762 <https://github.com/seven2762>
2025-12-03 19:25:26 +0900

    [feat] 패키지 분리로 인한 경로 변경

--- src/main/java/com/project/chaechaeserver/application/service/chatbot/ChatbotService.java
+++
@@ -1,9 +0,0 @@
-package com.project.chaechaeserver.application.service.chatbot;
-
-import com.project.chaechaeserver.application.response.chatbot.ResChatbotPostAnswerDTO;
-import com.project.chaechaeserver.presentation.request.chatbot.ReqChatbotPostQuestionDTO;
-
-public interface ChatbotService {
-
-    ResChatbotPostAnswerDTO ask(ReqChatbotPostQuestionDTO dto);
-}
--- src/main/java/com/project/chaechaeserver/application/service/user/InternalUserService.java
+++
@@ -1,9 +0,0 @@
-package com.project.chaechaeserver.application.service.user;
-
-import com.project.chaechaeserver.application.response.user.ResInternalUserPostSignupDTO;
-import com.project.chaechaeserver.presentation.request.user.ReqInternalUserPostSignupDTO;
-
-public interface InternalUserService {
-
-    ResInternalUserPostSignupDTO signup(ReqInternalUserPostSignupDTO dto);
-}
